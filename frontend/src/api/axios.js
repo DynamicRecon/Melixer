2bf4f286c9a4de0caf3d28b6817e9baf6d19cb0f
Scott Rose <vsrose2014@gmail.com>
2026-06-13 10:23:56 -0400

    staging production changes to backend server on the apache machine.

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -5,7 +5,7 @@ var hostname = window.location.hostname;
 const isLocalhost = ['localhost', '127.0.0.1'].includes(hostname);
 
 const API = axios.create({
-    baseURL: isLocalhost ? 'http://127.0.0.1:8000/Melixer' : "http://ExperimentsDB:8000",
+    baseURL: isLocalhost ? 'http://127.0.0.1:8000/Melixer' : "http://Melixer.local:8000",
 });
 
 export default API;
