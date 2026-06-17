0f54551538be7e5e1abacf81a17418d02b175ed3
Scott Rose <vsrose2014@gmail.com>
2026-06-17 16:49:02 -0400

    flipped around some backwards urls in production.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,9 +94,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",   # Create React App
     "http://localhost:5173",   # Vite
     "http://127.0.0.1:5173",   # Vite
-    "http://melixer.roseweb.local:3443", #server request.
-    "http://roseweb.local:3443",
-    "http://10.0.0.249:3443"
+    "https://melixer.roseweb.local", #server request.
+    "https://10.0.0.249"
 ]
 
 # Tell Django it's behind HTTPS
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -5,7 +5,7 @@ var hostname = window.location.hostname;
 const isLocalhost = ['localhost', '127.0.0.1'].includes(hostname);
 
 const API = axios.create({
-    baseURL: isLocalhost ? 'http://127.0.0.1:8000/Melixer' : "https://melixer.roseweb.local/Melixer",
+    baseURL: isLocalhost ? 'http://127.0.0.1:8000/Melixer' : "https://melixer.roseweb.local:3443/Melixer",
 });
 
 export default API;
